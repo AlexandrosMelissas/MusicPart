553de6ef6a4a42437354e809ab6d2599fde87e2e
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-22 21:38:14 +0200

    Init

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,10 +23,14 @@ app.use(history({
     verbose: true
    }))
 
+
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/../public/'))
 
-    app.get('/', (req, res) => { 
+    
+
+
+    app.get('/.*/', (req, res) => { 
         res.sendFile(__dirname + '/../public/index.html'); 
     });
 
