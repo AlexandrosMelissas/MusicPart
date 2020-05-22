2aee5baf30ed48b11706bd9cc7610acd142e41c4
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-22 21:59:38 +0200

    Init

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV === 'production') {
     }))
     
     app.get('/.*/', (req, res) => { 
-        res.sendFile(__dirname + '/../public/index.html'); 
+        res.sendFile(__dirname + '../public/index.html'); 
     });
 
 } else {
