9bc789e83a8c24366795968bf197171d07f64f53
AlexandrosMelissas <melissasalexandros@gmail.com>
2020-05-22 18:33:18 +0200

    init

--- backend/src/models/song.js
+++ backend/src/models/song.js
@@ -23,7 +23,8 @@ const songSchema = new mongoose.Schema({
     },
     image : {
         type: Buffer,
-        default: fs.readFileSync('./public/img/background.f925f5c9.jpg')
+        default: ''
+        // default: fs.readFileSync('./public/img/background.f925f5c9.jpg')
     },
     file: {
         type: Buffer,
